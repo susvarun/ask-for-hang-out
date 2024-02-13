efb431c3338090c0dc02b4c6d1984cb725ade033
susvarun <153448507+susvarun@users.noreply.github.com>
2024-02-13 09:49:13 +0000

    gradient added...

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function playVideo(choice) {
     const videoPlayer = document.getElementById('videoPlayer');
+    const plzImg = document.getElementById('plzImg')
     if (choice === 'yes') {
         videoPlayer.src = './YesClip.mp4'; // Set the video source for "Yes"
     } else if (choice === 'no') {
@@ -7,4 +8,5 @@ function playVideo(choice) {
     }
     videoPlayer.style.display = 'block'; // Show the video player
     videoPlayer.play(); // Start playing the selected video
+    plzImg.style.display = 'none';
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+const documentHeight = document.documentElement.clientHeight;
+const documentWidth = document.documentElement.clientWidth;
+console.log(`Document height: ${documentHeight}px, Document width: ${documentWidth}px`);
